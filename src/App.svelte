e1307e1427c00f88fa9d445289d4b1dc8bbfc423
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-04-29 16:51:38 -0400

    Revert "tge"
    This reverts commit 481be6743cdca8696a45f14d8caccd463a24ea51.

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,30 @@
 <script>
-    var urlParams = new URLSearchParams(location.search);
-    var vidRL = urlParams.get("v"); // https%3A%2F%2Fwww.w3schools.com%2Fhtml%2Fmov_bbb.mp4
-    var videoElement;
-
     export let name;
 </script>
 
-<video bind:this={vidRL}>
-    <source src={vidRL} type="video/mp4" />
-    <track kind="captions" />
-</video>
-
-<br />
-<button on:click={function() {
-    if (videoElement.paused) videoElement.play();
-    else videoElement.pause();
-}}>play/pause</button>
-
-<br />
-<input
-    type="range"
-    min={0}
-    max={videoElement.duration}
-    bind:value={videoElement.currentTime}
-    />
+<main>
+    <h1>Hello {name}!</h1>
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
 
 <style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
